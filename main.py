d9940ae7e5ab6667b9dbd43bba103fbde308b61a
Joao Haddad <joao.haddad@gmail.com>
2022-12-01 14:28:20 -0500

    Rewritten application

--- main.py
+++ main.py
@@ -1,128 +1,132 @@
 ####################################################################
 # Azure reader for Python
+# Performance Metrics being read: Available Memory Bytes, Percentage CPU
 ####################################################################
 
 from azure.identity import AzureCliCredential
-from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.monitor import MonitorManagementClient
-import os
-import pickle
-from datetime import date
+from azure.mgmt.subscription import SubscriptionClient
+from azure.mgmt.compute import ComputeManagementClient
+import json
 import datetime
-from dataclasses import dataclass
-from typing import List
+import lzma
+import uuid
 
-# PLEASE UPDATE THE SUBSCRIPTION_ID BELOW:
-SUBSCRIPTION_ID = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-FILENAME_METRICS = "savedmetricsdata.bin"
-FILENAME_VMLIST = "savedvmlistdata.bin"
+FILENAME = "saveddata.bin.lzma"
+VERSION = 2
+METRICS_TO_READ = ["Percentage CPU", "Available Memory Bytes"]
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
+
 
 # Acquire a credential object using CLI-based authentication.
 credential = AzureCliCredential()
+sub_client = SubscriptionClient(credential)
 
-# Retrieve subscription ID from environment variable.
-from azure.mgmt.compute import ComputeManagementClient
-
-vm_list = []
-
-@dataclass
-class Virtual_Machine_Type:
-    name: str
-    location: str
-    id: str
-    plan: str
-    vm_size: str
-    os_type: str
-    create_date: date
-
-####################################################################
-#Get the VM List, only keeping the attributes above
-####################################################################
-
-print ("Reading the VM List")
-
-compute_client = ComputeManagementClient(credential, SUBSCRIPTION_ID)
+saveddata = {}
 try:
-    for vm in compute_client.virtual_machines.list_all():
-        temp = vm.as_dict()
-        os_type = "Other"
-        if (vm.os_profile.linux_configuration != None):
-            os_type = "Linux"
-        if (vm.os_profile.windows_configuration != None):
-            os_type = "Windows"
-        vm_list.append(Virtual_Machine_Type(vm.name, vm.location, vm.id, vm.plan, vm.hardware_profile.vm_size, os_type, vm.time_created))
-except:
-    print (f"Error getting the VM List. Please check your Azure's Subscription ID. The one tried was {SUBSCRIPTION_ID}")
+    subs = [sub.as_dict() for sub in sub_client.subscriptions.list()]
+except Exception as e:
+    print(f"Error: {e}")
     exit()
-print (f"Total VM Count = {len(vm_list)}")
 
-print (vm_list)
+if len(subs) == 0:
+    print("No subscriptions available for your user.")
+    exit()
 
-####################################################################
-#Get the performance metrics
-####################################################################
-
-@dataclass
-class Time_Stamp:
-    pooling_time: date
-    result: float
-
-@dataclass
-class Metric_Type:
-    metric_name: str
-    metric_unit: str
-    results: List[Time_Stamp]
-
-
-@dataclass
-class VM_Metrics:
-    vm_info: Virtual_Machine_Type
-    results: List[Metric_Type]
-
-print ("\nGetting the Performance Metrics for the VMs")
-
-monitor_client = MonitorManagementClient(credential, SUBSCRIPTION_ID)
-
+print("Please inform your company name: ", end="")
+company_name = input()
+saveddata["subscriptions"] = subs
+vm_list = []
+vm_metrics = []
 today = datetime.datetime.utcnow()
 last_month = today - datetime.timedelta(days=30)
-vm_metrics = []
 
-for vm in vm_list:
-    resource_id = vm.id
-    metrics_data = monitor_client.metrics.list(
-        resource_id,
-        timespan="{}/{}".format(last_month, today),
-        interval='PT5M',
-        metricnames='Percentage CPU',
-        aggregation='Average'
-    )
-    metrics = []
+for sub in subs:
+    temp_vm_list = []
+    temp_vm_metrics = []
+    subscription_id = sub["subscription_id"]
 
-    for item in metrics_data.value:
-        # azure.mgmt.monitor.models.Metric
-        for timeserie in item.timeseries:
-            times = []
-            for data in timeserie.data:
-                # azure.mgmt.monitor.models.MetricData
-                if (data.average == None):
-                    continue
-                times.append (Time_Stamp(data.time_stamp, data.average))
-            
-            metrics.append (Metric_Type(item.name.localized_value,item.unit, times ))
-    vm_metrics.append(VM_Metrics(vm, metrics))
+    print(f"Reading the VM List for subscription {subscription_id}")
+    compute_client = ComputeManagementClient(credential, subscription_id)
+    try:
+        for vm in compute_client.virtual_machines.list_all():
+            vmdict = vm.as_dict()
+            vmdict["subscription"] = subscription_id
+            temp_vm_list.append(vmdict)
+    except:
+        print(
+            f"Error getting the VM List. Please check your Azure's Subscription ID. The one tried was {subscription_id}"
+        )
+        exit()
+    print(f"Total VM Count = {len(temp_vm_list)}")
 
-print ("\nPerformance Metrics finished. Saving the data")
+    vm_list.extend(temp_vm_list)
 
+    ####################################################################
+    # Get the performance metrics
+    ####################################################################
+
+    print("\nGetting VMs' Performance Metrics")
+    monitor_client = MonitorManagementClient(credential, subscription_id)
+
+    for vm in temp_vm_list:
+        resource_id = vm["id"]
+        metrics = []
+        for metric_type in METRICS_TO_READ:
+
+            metrics_data = monitor_client.metrics.list(
+                resource_id,
+                timespan="{}/{}".format(last_month, today),
+                interval="PT15M",
+                metricnames=metric_type,
+                aggregation="Average",
+            )
+
+            for item in metrics_data.value:
+                # azure.mgmt.monitor.models.Metric
+                for timeserie in item.timeseries:
+                    times = []
+                    for data in timeserie.data:
+                        # azure.mgmt.monitor.models.MetricData
+                        if data.average == None:
+                            continue
+                        times.append(
+                            {"time_stamp": data.time_stamp, "average": data.average}
+                        )
+
+                    metrics.append(
+                        {
+                            "localized_value": item.name.localized_value,
+                            "unit": item.unit,
+                            "times": times,
+                        }
+                    )
+        temp_vm_metrics.append({"vm": vm, "metrics": metrics})
+    vm_metrics.extend(temp_vm_metrics)
+
+saveddata["metrics"] = vm_metrics
+saveddata["vm_list"] = vm_list
+saveddata["version"] = VERSION  # With the new format
+saveddata["processed_date"] = today
+saveddata["id"] = str(uuid.uuid4())
+saveddata["customer_name"] = company_name
+saveddata["csp"] = "Azure"
+
+print("\nPerformance Metrics finished. Saving the data")
 
 # Save to the directory
 try:
+    with lzma.open(FILENAME, "wt", encoding="ascii") as f:
+        json.dump(saveddata, f, default=str)
 
-    with open(FILENAME_METRICS, 'wb') as f:
-        pickle.dump(vm_metrics, f, pickle.HIGHEST_PROTOCOL)
-
-    with open(FILENAME_VMLIST, 'wb') as f:
-        pickle.dump(vm_list, f, pickle.HIGHEST_PROTOCOL)
-except:
-    print (f"Error saving the files \"{FILENAME_METRICS}\" and \"{FILENAME_VMLIST}\". Do you have write access?")
+except Exception as E:
+    print(f'Error saving file "{FILENAME}". Error = {E}')
     exit()
-print (f"Saved successfully. The saved files are \"{FILENAME_METRICS}\" and \"{FILENAME_VMLIST}\"")
+print(f'Report saved successfully to "{FILENAME}"')
