8afb2e064d15b93a43328b4bf29111048d2313e3
Joao Haddad <joao.haddad@gmail.com>
2023-02-14 08:37:41 -0500

    Included Azure Reservations

--- main.py
+++ main.py
@@ -3,17 +3,21 @@
 # Performance Metrics being read: Available Memory Bytes, Percentage CPU
 ####################################################################
 
-from azure.identity import AzureCliCredential
-from azure.mgmt.monitor import MonitorManagementClient
-from azure.mgmt.subscription import SubscriptionClient
-from azure.mgmt.compute import ComputeManagementClient
-import json
 import datetime
+import json
 import lzma
+import sys
 import uuid
 
+from azure.core.exceptions import AzureError
+from azure.identity import AzureCliCredential
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.monitor import MonitorManagementClient
+from azure.mgmt.reservations import AzureReservationAPI
+from azure.mgmt.subscription import SubscriptionClient
+
 FILENAME = "saveddata.bin.lzma"
-VERSION = 2
+VERSION = 3
 METRICS_TO_READ = ["Percentage CPU", "Available Memory Bytes"]
 
 
@@ -22,23 +26,24 @@ def json_serial(obj):
 
     if isinstance(obj, (datetime.datetime, datetime.date)):
         return obj.isoformat()
-    raise TypeError("Type %s not serializable" % type(obj))
+    raise TypeError(f"Type { type(obj) } not serializable")
 
 
 # Acquire a credential object using CLI-based authentication.
 credential = AzureCliCredential()
 sub_client = SubscriptionClient(credential)
 
+
 saveddata = {}
 try:
     subs = [sub.as_dict() for sub in sub_client.subscriptions.list()]
-except Exception as e:
+except AzureError as e:
     print(f"Error: {e}")
-    exit()
+    sys.exit()
 
 if len(subs) == 0:
     print("No subscriptions available for your user.")
-    exit()
+    sys.exit()
 
 print("Please inform your company name: ", end="")
 company_name = input()
@@ -60,11 +65,11 @@ for sub in subs:
             vmdict = vm.as_dict()
             vmdict["subscription"] = subscription_id
             temp_vm_list.append(vmdict)
-    except:
+    except AzureError as e:
         print(
             f"Error getting the VM List. Please check your Azure's Subscription ID. The one tried was {subscription_id}"
         )
-        exit()
+        sys.exit()
     print(f"Total VM Count = {len(temp_vm_list)}")
 
     vm_list.extend(temp_vm_list)
@@ -75,41 +80,63 @@ for sub in subs:
 
     print("\nGetting VMs' Performance Metrics")
     monitor_client = MonitorManagementClient(credential, subscription_id)
-
     for vm in temp_vm_list:
         resource_id = vm["id"]
         metrics = []
         for metric_type in METRICS_TO_READ:
+            try:
 
-            metrics_data = monitor_client.metrics.list(
-                resource_id,
-                timespan="{}/{}".format(last_month, today),
-                interval="PT15M",
-                metricnames=metric_type,
-                aggregation="Average",
-            )
+                metrics_data = monitor_client.metrics.list(
+                    resource_id,
+                    timespan="{}/{}".format(last_month, today),
+                    interval="PT15M",
+                    metricnames=metric_type,
+                    aggregation="Average",
+                )
 
-            for item in metrics_data.value:
-                # azure.mgmt.monitor.models.Metric
-                for timeserie in item.timeseries:
-                    times = []
-                    for data in timeserie.data:
-                        # azure.mgmt.monitor.models.MetricData
-                        if data.average == None:
-                            continue
-                        times.append(
-                            {"time_stamp": data.time_stamp, "average": data.average}
+                for item in metrics_data.value:
+                    # azure.mgmt.monitor.models.Metric
+                    for timeserie in item.timeseries:
+                        times = []
+                        for data in timeserie.data:
+                            # azure.mgmt.monitor.models.MetricData
+                            if data.average is None:
+                                continue
+                            times.append(
+                                {"time_stamp": data.time_stamp, "average": data.average}
+                            )
+
+                        metrics.append(
+                            {
+                                "localized_value": item.name.localized_value,
+                                "unit": item.unit,
+                                "times": times,
+                            }
                         )
+            except AzureError as e:
+                continue
 
-                    metrics.append(
-                        {
-                            "localized_value": item.name.localized_value,
-                            "unit": item.unit,
-                            "times": times,
-                        }
-                    )
         temp_vm_metrics.append({"vm": vm, "metrics": metrics})
     vm_metrics.extend(temp_vm_metrics)
+
+print("\nPerformance Metrics finished")
+
+# Collect Reservation metrics
+
+sub_client = SubscriptionClient(credential)
+saveddata = {}
+try:
+    reservationclient = AzureReservationAPI(credential)
+    saveddata["reservation"] = [
+        item.as_dict() for item in reservationclient.reservation.list_all()
+    ]
+except AzureError as e:
+    print("Azure Error: %s", e)
+
+if "reservation" in saveddata:
+    print("\nReservation Metrics were found")
+else:
+    print("\nNo Reservation Metrics were found\n")
 
 saveddata["metrics"] = vm_metrics
 saveddata["vm_list"] = vm_list
@@ -119,14 +146,14 @@ saveddata["id"] = str(uuid.uuid4())
 saveddata["customer_name"] = company_name
 saveddata["csp"] = "Azure"
 
-print("\nPerformance Metrics finished. Saving the data")
-
 # Save to the directory
 try:
     with lzma.open(FILENAME, "wt", encoding="ascii") as f:
         json.dump(saveddata, f, default=str)
-
-except Exception as E:
-    print(f'Error saving file "{FILENAME}". Error = {E}')
-    exit()
+except lzma.LZMAError as e:
+    print(f'Error saving file "{FILENAME}". Error = {e}')
+    sys.exit()
+except json.JSONDecodeError as exception:
+    print(f"Error: {exception}")
+    sys.exit()
 print(f'Report saved successfully to "{FILENAME}"')
